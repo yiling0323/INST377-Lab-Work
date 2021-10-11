1fe794ef05aa2701729604d3d74e33e142abc1d5
yiling0323 <duyiling0323@gmail.com>
2021-10-11 19:14:49 -0400

    change dataHandler, windowsAction, mapInit functions lab7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,20 +1,22 @@
-async function windowActions(evt) {
-  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; /* '/api/wholeMeal' */
+async function dataHandler(mymap) {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   const request = await fetch(endpoint);
   const food = await request.json();
   /* console.log(food); */
 
   function findMatches(wordToMatch, food) {
     return food.filter((place) => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.zip.match(regex);
+      if (wordToMatch.length === 5) {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.zip.match(regex);
+      }
     });
   }
 
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, food);
-    const matchArrayFive = matchArray.slice(0, 5);
-    /* console.log(matchArrayFive) */
+    const matchArrayFive = matchArray.filter((item) => item.geocoded_column_1 != null).slice(0, 5);
+    /* console.log(matchArrayFive); */
     const html = matchArrayFive.map((place) => `
           <span class="info">
               <span class="name">${place.name}</span><br/>
@@ -23,6 +25,13 @@ async function windowActions(evt) {
               <br/>
           `).join('');
     suggestions.innerHTML = html;
+
+    /* for (item of matchArrayFive) {} */
+    matchArrayFive.forEach((item, index) => {
+      const geo = item.geocoded_column_1.coordinates.reverse();
+      /* console.log(geo); */
+      const marker = L.marker(geo).addTo(mymap);
+    });
   }
 
   const searchInput = document.querySelector('.search');
@@ -31,27 +40,24 @@ async function windowActions(evt) {
   searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 }
 
-window.onload = windowActions;
-
-/*
 function mapInit() {
- const mymap = L.map('mapid').setView([39.023, -76.925], 13);
+  const mymap = L.map('mapid').setView([38.966, -76.910], 11);
 
- L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZHV5aWxpbmciLCJhIjoiY2t1bHg3eWVkMDU5cDJ1bGM5dDgwcGxhaiJ9.gMQBgYz5baqA3ILises0mQ'
-}).addTo(mymap);
+  }).addTo(mymap);
 
-displayMatches(event).forEach((item, index) => {
-      const geo = [item.geocoded_column_1].reverse();
-      var marker = L.marker(geo).addTo(mymap);
-    });
+  return mymap;
 }
 
-var marker = L.marker([39.023, -76.925]).addTo(mymap);
+async function windowActions() {
+  mymap = mapInit();
+  await dataHandler(mymap);
+}
 
-*/
+window.onload = windowActions;
