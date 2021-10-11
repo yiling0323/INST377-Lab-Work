99e9f72f64f51c5f433d841a4ac0fb3a0d7c2ebd
yiling0323 <duyiling0323@gmail.com>
2021-10-10 23:15:55 -0400

    show only five locations lab7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -2,11 +2,7 @@ async function windowActions(evt) {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; /* '/api/wholeMeal' */
   const request = await fetch(endpoint);
   const food = await request.json();
-  /*console.log(food);*/
-  /*
-    fetch(endpoint)
-      .then((blob) => blob.json())
-      .then((data) => food.push(...data)); */
+  /* console.log(food); */
 
   function findMatches(wordToMatch, food) {
     return food.filter((place) => {
@@ -17,16 +13,14 @@ async function windowActions(evt) {
 
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, food);
-    const html = matchArray.map((place) => `
+    const matchArrayFive = matchArray.slice(0, 5);
+    /* console.log(matchArrayFive) */
+    const html = matchArrayFive.map((place) => `
           <span class="info">
               <span class="name">${place.name}</span><br/>
-              
               <span class="address">${place.address_line_1}</span><br/>
-              
-              
               </span>
               <br/>
-          
           `).join('');
     suggestions.innerHTML = html;
   }
@@ -38,9 +32,10 @@ async function windowActions(evt) {
 }
 
 window.onload = windowActions;
+
 /*
 function mapInit() {
- const mymap = L.map('mapid').setView([39.023, -76.925], 13); 
+ const mymap = L.map('mapid').setView([39.023, -76.925], 13);
 
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -51,5 +46,12 @@ function mapInit() {
     accessToken: 'pk.eyJ1IjoiZHV5aWxpbmciLCJhIjoiY2t1bHg3eWVkMDU5cDJ1bGM5dDgwcGxhaiJ9.gMQBgYz5baqA3ILises0mQ'
 }).addTo(mymap);
 
+displayMatches(event).forEach((item, index) => {
+      const geo = [item.geocoded_column_1].reverse();
+      var marker = L.marker(geo).addTo(mymap);
+    });
+}
+
 var marker = L.marker([39.023, -76.925]).addTo(mymap);
-}*/
+
+*/
